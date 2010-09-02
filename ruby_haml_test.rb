eba7d32df4554bff7db4928ace172ac1457d3955
enosis <enosis@github.com>
2010-09-02 00:43:33 -0700

    Workaround to just about cope with Ruby 1.8.7 + JSON 1.4.3

--- ruby_haml_test.rb
+++ ruby_haml_test.rb
@@ -8,10 +8,10 @@ class HamlTest < Test::Unit::TestCase
     context[1].each do |name, test|
       class_eval(<<-EOTEST)
         def test_#{name.gsub(/\s+|[^a-zA-Z0-9_]/, "_")}
-          locals = Hash[*(#{test}["locals"] || {}).collect {|k, v| [k.to_sym, v] }.flatten]
-          options = Hash[*(#{test}["config"] || {}).collect {|k, v| [k.to_sym, v.to_sym] }.flatten]
-          engine = Haml::Engine.new(#{test}["haml"], options)
-          assert_equal(#{test}["html"], engine.render(Object.new, locals).chomp)
+          locals = Hash[*(#{test["locals"].inspect} || {}).collect {|k, v| [k.to_sym, v] }.flatten]
+          options = Hash[*(#{test["config"].inspect} || {}).collect {|k, v| [k.to_sym, v.to_sym] }.flatten]
+          engine = Haml::Engine.new(%q^#{test["haml"]}^, options)
+          assert_equal(%q^#{test["html"]}^, engine.render(Object.new, locals).chomp)
         end
       EOTEST
     end
